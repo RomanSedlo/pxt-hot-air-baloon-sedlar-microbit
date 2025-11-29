06f4adaecf6e694fe78acdcdcc0ac2ced1bc2508
Roman Sedlář <roman.sedlar.024@pslib.cz>
2025-11-29 10:47:34 +0100

    menu and fixes

--- main.ts
+++ main.ts
@@ -1,37 +1,37 @@
 enum difficulty { EASY, MEDIUM, HARD };
 
 type level = {
-    order: number,
     dist: number,
     dif: difficulty
 };
 
-
 const levels: Array<level> = [
-    { order: 1, dist: 20, dif: difficulty.EASY },
-    { order: 2, dist: 40, dif: difficulty.EASY },
-    { order: 3, dist: 60, dif: difficulty.EASY },
-    { order: 1, dist: 30, dif: difficulty.MEDIUM },
-    { order: 2, dist: 50, dif: difficulty.MEDIUM },
-    { order: 3, dist: 100, dif: difficulty.MEDIUM },
-    { order: 1, dist: 40, dif: difficulty.HARD },
-    { order: 2, dist: 60, dif: difficulty.HARD },
-    { order: 3, dist: 120, dif: difficulty.HARD }
+    { dist: 20, dif: difficulty.EASY },
+    { dist: 40, dif: difficulty.EASY },
+    { dist: 60, dif: difficulty.EASY },
+    { dist: 40, dif: difficulty.MEDIUM },
+    { dist: 80, dif: difficulty.MEDIUM },
+    { dist: 120, dif: difficulty.MEDIUM },
+    { dist: 50, dif: difficulty.HARD },
+    { dist: 100, dif: difficulty.HARD },
+    { dist: 200, dif: difficulty.HARD }
 ]
 
+let selectedLevel: level;
+let chosenLevel: number = 0;
+let shownLevel: number = 0;
+let startSignal: boolean = false;
 
-let chosenLevel: level;
-
-let brightness: number = 120
-let obrightness: number = brightness - 30;
-let rollSpeed: number = 60
-let cframe: number = 100
+const brightness: number = 120
+const obrightness: number = brightness - 30;
+const rollSpeed: number = 60
+const cframe: number = 100
 
 let player: game.LedSprite = game.createSprite(2, 4);
 player.set(LedSpriteProperty.Brightness, brightness);
 
 let stop: boolean = false;
-let inMenus: boolean = true;
+let inMenu: boolean = true;
 let inLevel: boolean = false;
 let inGameOver: boolean = false;
 
@@ -40,31 +40,50 @@ let difChance: number;
 let difRest: number;
 
 input.onButtonPressed(Button.A, function () {
-    if (inLevel) {
+    if (inMenu) {
+        if (shownLevel > 0) {
+            shownLevel -= 1
+        }
+    } else if (inLevel) {
         player.move(-1)
     }
-
 })
 
 input.onButtonPressed(Button.B, function () {
-    if (inLevel) {
+    if (inMenu) {
+        if (shownLevel < levels.length - 1) {
+            shownLevel += 1
+        }
+    } else if (inLevel) {
         player.move(1)
-    } else if (inGameOver) {
-        stop = false
-        inGameOver = false
-
-        player.set(LedSpriteProperty.X, 2)
-
-        inLevel = true
-        control.inBackground(function () {
-            play(chosenLevel)
-        })
     }
 })
+
+input.onButtonPressed(Button.AB, function () {
+    if (inMenu || inGameOver) {
+        startSignal = true
+    }
+})
+
+function menu(): number {
+    inMenu = true
+    player.set(LedSpriteProperty.Brightness, 0)
+    if (!startSignal) {
+        menuLoop: while (true) {
+            basic.showNumber(shownLevel + 1, 50)
+            basic.pause(50)
+            if (startSignal) {
+                break menuLoop;
+            }
+        }
+    }
+    return shownLevel
+}
 
 function gameover(level: level) {
     inLevel = false
     stop = true
+    startSignal = false
     control.inBackground(function () {
         //music.playMelody("A4 E3 C3 A3 - A2 - A2", 260)
     })
@@ -82,25 +101,24 @@ function gameover(level: level) {
 function spawn(level: level, index: number) {
     player.set(LedSpriteProperty.Brightness, brightness);
     if (level.dif === difficulty.EASY) {
-        difChance = 6
+        difChance = 5
         difSpeed = 9
         difRest = 500
     } else if (level.dif === difficulty.MEDIUM) {
-        difChance = 5
+        difChance = 4
         difSpeed = 8
         difRest = 300
     } else if (level.dif === difficulty.HARD) {
-        difChance = 4
+        difChance = 3
         difSpeed = 7
         difRest = 200
     }
-    for (let x = 0; x < 5; x += 1) {
-        if (stop === true) {
-            break;
+    levelLoop: for (let x = 0; x < 5; x += 1) {
+        if (inGameOver) {
+            break levelLoop;
         }
-        basic.pause((level.dist - index) * difSpeed)
-        let chance = difChance - level.order
-        if (Math.randomRange(0, chance - 1) == 1) {
+        basic.pause(difRest / 10)
+        if (Math.randomRange(0, difChance - 1) == 1) {
             let o = game.createSprite(x, 0)
             o.set(LedSpriteProperty.Brightness, obrightness)
             control.inBackground(function () {
@@ -119,27 +137,37 @@ function spawn(level: level, index: number) {
                 }
             })
         }
-        basic.pause(200)
+        basic.pause(((level.dist - index) / difSpeed) + (difRest))
     }
 }
 
 function play(level: level) {
     inLevel = true
-    for (let i: number = 0; i < level.dist; i += 1) {
+    playLoop: for (let i: number = 0; i < level.dist; i += 1) {
         spawn(level, i)
         if (stop) {
-            break;
+            break playLoop;
+        }
+        if (i === level.dist - 1) {
+            basic.pause(2500)
+            basic.showString("YOU WON!", rollSpeed * 1.2)
         }
     }
-    basic.pause(2400)
-    basic.showString("YOU WON!", rollSpeed * 1.2)
     stop = true
+    startSignal = false
 }
 
+// basic.showString('HOT AIR BALOON GAME', rollSpeed / 1.25)
 while (true) {
-    chosenLevel = levels[0]
-    play(chosenLevel)
-    basic.pause(cframe)
+    chosenLevel = menu()
+    if (startSignal) {
+        inMenu = false
+        stop = false
+        inGameOver = false
+        player.set(LedSpriteProperty.X, 2)
+        player.set(LedSpriteProperty.Brightness, brightness)
+        inLevel = true
+        selectedLevel = levels[chosenLevel]
+        play(selectedLevel)
+    }
 }
-
-
